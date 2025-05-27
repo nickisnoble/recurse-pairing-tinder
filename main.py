63f16cfb938fcc8c96fab96223e14ab6b73e61c5
Nick Noble <nick@miniware.team>
2025-05-27 12:35:44 -0400

    sqlite!

--- main.py
+++ main.py
@@ -1,16 +1,82 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import Depends, FastAPI, HTTPException, Query
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+class User(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=False)
+    email: str = Field(index=True, unique=True)
+
+class Project(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True, unique=True)
+    description: str = Field()
+
+class Tag(SQLModel, table=True):
+    label: str = Field(index=True, primary_key=True)
+
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+connect_args = {"check_same_thread": False}
+engine = create_engine(sqlite_url, connect_args=connect_args)
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
 
 app = FastAPI()
+
+@app.on_event("startup")
+def on_startup():
+    create_db_and_tables()
 
 @app.get("/")
 async def root():
     return {"message": "Hello from pairing-tinder!"}
 
 
+@app.post("/projects/")
+def create_project(project: Project, session: SessionDep) -> Project:
+    session.add(project)
+    session.commit()
+    session.refresh(project)
+    return project
 
-def main():
-    print("Hello from pairing-tinder!")
+
+@app.get("/projects/")
+def read_projects(
+    session: SessionDep,
+    offset: int = 0,
+    limit: Annotated[int, Query(le=100)] = 100,
+) -> list[Project]:
+    projects = session.exec(select(Project).offset(offset).limit(limit)).all()
+    return projects
 
 
-if __name__ == "__main__":
-    main()
+@app.get("/projects/{project_id}")
+def read_project(project_id: int, session: SessionDep) -> Project:
+    project = session.get(Project, project_id)
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+    return project
+
+
+@app.delete("/projects/{project_id}")
+def delete_project(project_id: int, session: SessionDep):
+    project = session.get(Project, project_id)
+    if not project:
+        raise HTTPException(status_code=404, detail="Project not found")
+    session.delete(project)
+    session.commit()
+    return {"ok": True}
