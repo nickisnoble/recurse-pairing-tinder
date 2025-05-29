c4b399babd3caa58225905df7e1a5368d0a14cfa
Nick Noble <nick@miniware.team>
2025-05-29 14:54:17 -0400

    add alt text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
   import LogoUrl from "$lib/assets/logo.svg"
 </script>
 <div class="p-6 flex flex-col h-screen items-center justify-center">
-  <img src={LogoUrl} class="size-64" />
+  <img src={LogoUrl} alt="RC Swipe Logo" class="size-64" />
 
   <h1 class="font-bold text-2xl text-center">Welcome to RC Swipe</h1>
 </div>
